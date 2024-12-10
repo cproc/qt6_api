41715cd60357244a32e417dafbeba05a77718ee5
Christian Prochaska <christian.prochaska@genode-labs.com>
2024-12-10 16:30:52 +0100

    qtdeclarative_genode_api_generated.patch

---
+++ lib/cmake/Qt6QmlToolingSettingsPrivate/Qt6QmlToolingSettingsPrivateTargets-relwithdebinfo.cmake
@@ -0,0 +1,18 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "Qt6::QmlToolingSettingsPrivate" for configuration "RelWithDebInfo"
+set_property(TARGET Qt6::QmlToolingSettingsPrivate APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(Qt6::QmlToolingSettingsPrivate PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "CXX"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libQt6QmlToolingSettings.lib.so"
+  )
+
+list(APPEND _IMPORT_CHECK_FILES_FOR_Qt6::QmlToolingSettingsPrivate "${_IMPORT_PREFIX}/lib/libQt6QmlToolingSettings.lib.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
